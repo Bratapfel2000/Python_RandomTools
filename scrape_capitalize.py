b65598c3412d656283660873f5756d9c3bf3bdee
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2018-10-18 17:01:50 +0200

    Add files via upload

---
+++ scrape_capitalize.py
@@ -0,0 +1,23 @@
+#scrapes a word and capitalizises it/them
+
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import random
+
+#scrapes a word
+source = requests.get('---').text
+soup = BeautifulSoup(source, 'html.parser')
+eins = soup.find(id="---")
+zwei = eins.find_all(class_="---")
+drei = zwei[0]
+#print(drei.text)
+
+#waehlt zufaellig gut oder schlecht aus +impor random
+a = ["gut", "schlecht", "voll gut", "ziemlich fuer den Arsch", "naja", "geht so", "spitzenklasse",
+     "erste Sahne", "zweite Sahne", "voll Zehlendorf", "unangenehm", "gar nicht mal so gut",
+     "besser", "schlechter", "hipstermaessig"]
+
+#capitalize
+print(drei.text.title() + " ist " + random.choice(a) + ".")   
