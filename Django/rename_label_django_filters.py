e7b1d50cef0fbd66b452c103a3c2873e84b28774
Bratapfel <bratapfel2k@gmail.com>
2021-02-06 10:34:17 +0100

    added change name in dropdown django_filters

--- Django/rename_label_django_filters.py
+++ Django/rename_label_django_filters.py
@@ -1,5 +1,5 @@
+#changes only name of field. Not working with dropdown menu
 import django_filters
-
 from .models import *
 
 class ProductFilter(django_filters.FilterSet):
@@ -9,3 +9,17 @@ class ProductFilter(django_filters.FilterSet):
 		exclude = ['image']
 		#fields = '__all__'
 		fields =  ['field_1']
+
+
+
+
+#this works with dropdown
+class ProductFilter(django_filters.FilterSet):
+	LETTERS = (('KP', 'KP'),
+                   ('A', 'A'),)
+	field_1 = django_filters.ChoiceFilter(label='test123', choices=LETTERS)        
+	class Meta:
+		model = Product
+		exclude = ['image']
+		#fields = '__all__'
+		fields = {'field_1': ['exact'],}
